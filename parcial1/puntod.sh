c14dcc20fea1eba2ea5d6f114c7ca0faf4655584
belugomezr <belen.gomezrolon@gmail.com>
2024-10-17 20:33:14 +0000

    puntod.sh

--- parcial1/puntod.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p /home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcial1/Estructura_Asimetrica/{{correo,clientes}/cartas{1..50}, correo/carteros{1,10}}
-
-cd EstructuraAsimetrica
-
-EstructuraAsimetrica tree
-
-
