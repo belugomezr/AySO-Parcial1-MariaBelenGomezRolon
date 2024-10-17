909ea9a59d40ef3ad836d4ab3ba9eca558a311c3
belugomezr <belen.gomezrolon@gmail.com>
2024-10-17 20:30:46 +0000

    puntoa.sh

---
+++ parcial1/puntoa.sh
@@ -0,0 +1,7 @@
+mkdir -p /home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcial1/Examenes-UTN/{alumno_1/parcial_{1..3},alumno_2/parcial_{1..3},alumno_3/parcial_{1..3},profesores}
+
+
+tree Examenes-UTN
+
+
+
