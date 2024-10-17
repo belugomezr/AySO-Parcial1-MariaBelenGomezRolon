9091a3e6bfb1616a0f4e1f070935ae035a0dd21d
root <root@VMPruebas>
2024-10-17 18:12:26 +0000

    puntoa.sh

--- parcial1/puntoa.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p /home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcial1/Examenes-UTN/{alumno_1,alumno_2,alumno_3,profesores}/parcial_{1..3}
-
-tree Examenes-UTN
--- parcial1/puntoc.sh
+++ parcial1/puntoc.sh
@@ -10,6 +10,10 @@ sudo chown tester:tester /home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcia
 sudo chown analistas:analistas /home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcial1/Examenes-UTN/alumno_3
 sudo chown disenadores:disenadores /home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcial1/Examenes-UTN/profesores
 
+sudo chmod 750 /home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcial1/Examenes-UTN/alumno_1
+sudo chmod 750 /home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcial1/Examenes-UTN/alumno_2
+sudo chmod 700 /home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcial1/Examenes-UTN/alumno_3
+sudo chmod 775 /home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcial1/Examenes-UTN/profesores
 
 
 
