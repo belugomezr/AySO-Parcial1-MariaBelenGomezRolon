79e3c7f35734c88a4981dde643eea393979e13e0
belugomezr <belen.gomezrolon@gmail.com>
2024-10-17 13:59:59 +0000

    puntoa.sh

--- parcial1/puntoa.sh
+++ parcial1/puntoa.sh
@@ -1,3 +1,3 @@
-mkdir -p /home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcial1/Examenes-UTN/{alumno1,alumno2,alumno3,profesores}/parcial_{1..3}
+mkdir -p /home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcial1/Examenes-UTN/{alumno_1,alumno_2,alumno_3,profesores}/parcial_{1..3}
 
 tree Examenes-UTN
--- parcial1/puntoc.sh
+++ parcial1/puntoc.sh
@@ -4,5 +4,11 @@ sudo adduser analistas
 sudo adduser disenadores
 
 
-sudo chmod 750 home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcial1/Examenes-UTN/alumno_1
+
+sudo chown programadores:programadores /home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcial1/Examenes-UTN/alumno_1
+sudo chown tester:tester /home/vagrant/AySO-Parcial1-MariaBelenGomezRolon/parcial1/Examenes-UTN/alumno_2
+
+
+
+
 
